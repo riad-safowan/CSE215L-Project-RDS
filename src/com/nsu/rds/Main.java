c3eb97502fef24f561a2bac7a3b3f63f2cac2763
Riad Safowan <riad.safowan@northsouth.edu>
2021-12-28 19:56:16 +0600

    UI

--- src/com/nsu/rds/Main.java
+++ src/com/nsu/rds/Main.java
@@ -1,7 +1,9 @@
 package src.com.nsu.rds;
 
+import src.com.nsu.rds.ui.UI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        UI.showLoginScreen();
     }
 }
---
+++ src/com/nsu/rds/ui/UI.java
@@ -0,0 +1,12 @@
+package src.com.nsu.rds.ui;
+
+public class UI {
+    public static void showWelcome(){
+        System.out.println("Welcome to North South University\n\n");
+    }
+
+    public static void showLoginScreen(){
+        showWelcome();
+        System.out.println("Login");
+    }
+}
